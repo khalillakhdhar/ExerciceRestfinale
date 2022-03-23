9c5b53eb5b4e1f8fa19b63786157efa7969ec2ac
khalil <khalillakhdharatc@gmail.com>
2022-03-23 11:46:32 +0100

    Initial commit

---
+++ src/main/java/com/rest/ExerciceRestfinaleApplication.java
@@ -0,0 +1,13 @@
+package com.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciceRestfinaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciceRestfinaleApplication.class, args);
+	}
+
+}
